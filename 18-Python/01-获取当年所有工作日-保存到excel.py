f3dc3e316021e24739dfa36a0074c724e1df8f50
demonxinghen <demonxinghen@126.com>
2024-05-15 15:26:00 +0800

    feat: python获取当年所有工作日-格式化代码

--- 18-Python/01-获取当年所有工作日-保存到excel.py
+++ 18-Python/01-获取当年所有工作日-保存到excel.py
@@ -4,13 +4,14 @@ import pandas as pd
 from datetime import datetime
 import numpy as np
 
+
 # 从百度的php接口中获取到数据
-def catch_url_from_baidu(calcultaion_year, month):
+def catch_url_from_baidu(year, month):
     headers = {
         "Content-Type": "application/json;charset=UTF-8"
     }
     param = {
-        "query": calcultaion_year + "年" + month + "月",
+        "query": year + "年" + month + "月",
         "resource_id": "39043",
         "t": "1604395059555",
         "ie": "utf8",
@@ -23,56 +24,60 @@ def catch_url_from_baidu(calcultaion_year, month):
     r = requests.get(url="https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php",
                      headers=headers, params=param).text
     month_data = json.loads(r)["data"][0]["almanac"]
-    not_work_day = []  #非工作日
-    work_day = [] #工作日
+    work_day = []  # 工作日
     for one in month_data:
-        #工作日
-        if ('status' not  in one ):
-            if(one["cnDay"] != '日' and one["cnDay"] != '六'):
-                #正常工作日
+        # 工作日
+        if 'status' not in one:
+            if one["cnDay"] != '日' and one["cnDay"] != '六':
+                # 正常工作日
                 work_day.append(one)
-        if(one["cnDay"] == '日' or one["cnDay"] == '六'):
-            if ('status' in one):
+        if one["cnDay"] == '日' or one["cnDay"] == '六':
+            if 'status' in one:
                 # status为2的时候表示周末的工作日，比如10月10日。即百度工具左上角显示“班”的日期
-                if (one["status"] == "2"):
+                if one["status"] == "2":
                     work_day.append(one)
 
     return export_excel(work_day)
 
-#日期格式规范
-def addZero(num):
+
+# 日期格式规范
+def add_zero(num):
     if int(num) < 10:
-        return str( '0' + num)
+        return str('0' + num)
     else:
         return str(num)
 
+
 def export_excel(not_work_day):
     export_list = []
-    for one in  not_work_day:
-        month = addZero(one['month'])
-        day = addZero(one['day'])
-        export_list.append(one['year']+'-'+month+'-'+day)
+    for one in not_work_day:
+        month = add_zero(one['month'])
+        day = add_zero(one['day'])
+        export_list.append(one['year'] + '-' + month + '-' + day)
     return export_list
 
-#获取每个月的工作日
-def asMonth(date_list):
-    month_list = [1,2,3,4,5,6,7,8,9,10,11,12] #月份
+
+# 获取每个月的工作日
+def as_month(date_list):
+    month_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]  # 月份
     results_list = []
     for m in month_list:
         result_list = []
         j_dates = [date for date in date_list if datetime.strptime(date, '%Y-%m-%d').month == m]
-        if len(j_dates)>0:
-            result_list.append(str(m)+'月')
+        if len(j_dates) > 0:
+            result_list.append(str(m) + '月')
             result_list.append(j_dates)
             results_list.append(result_list)
     # print(results_list)
     return results_list
 
-#统一长度
-def sameLen(lst):
+
+# 统一长度
+def same_len(lst):
     new_lst = [np.nan] * 30
     new_lst[:len(lst)] = lst
     return new_lst
+
 
 if __name__ == '__main__':
     # 国务院是每年12月份才会发布第二年的放假计划
@@ -83,8 +88,8 @@ if __name__ == '__main__':
     df = pd.DataFrame()
     for one_month in calculation_month:
         e = catch_url_from_baidu(calculation_year, one_month)
-        for m_list in asMonth(e):
-            df[m_list[0]] = sameLen(m_list[1])
+        for m_list in as_month(e):
+            df[m_list[0]] = same_len(m_list[1])
     # 导出
     df.to_excel(r'E:\{}年工作日.xlsx'.format(calculation_year), index=False)
     df.to_csv(r'E:\{}年工作日.txt'.format(calculation_year), sep='\t', index=False)
